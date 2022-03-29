29e26310e2cbddc749f8c5c55f041a3812c708dd
cmj <819953059@qq.com>
2022-03-29 15:58:12 +0800

    epc列表去掉所有的操作按钮

--- src/views/Config/EPCList.vue
+++ src/views/Config/EPCList.vue
@@ -2,35 +2,13 @@
   <div class="epc-list">
     <div class="top">
       <h2>{{ supplierName }}</h2>
-      <a-button
-        style="margin-bottom: 20px"
-        type="primary"
-        @click="showModal('new')"
-        >新增EPC</a-button
-      >
     </div>
 
-    <a-table bordered :data-source="dataSource" :columns="columns">
-      <template slot="action" slot-scope="record">
-        <div class="actions">
-          <a @click="showModal('edit', record)">编辑</a>
-          <a-popconfirm
-            title="确定删除？"
-            ok-text="确定"
-            cancel-text="取消"
-            @confirm="confirmDelete(record.id)"
-          >
-            <a href="#" class="danger">删除</a></a-popconfirm
-          >
-        </div>
-      </template>
-    </a-table>
-    <EPCModal ref="EPCModal" :form="defaultForm" @handleOk="handleOk" />
+    <a-table bordered :data-source="dataSource" :columns="columns"></a-table>
   </div>
 </template>
 
 <script>
-import EPCModal from "../../components/EPCModal.vue";
 const columns = [
   {
     title: "序号",
@@ -50,14 +28,8 @@ const columns = [
     key: "epc_name",
     ellipsis: true,
   },
-  {
-    title: "操作",
-    key: "operation",
-    scopedSlots: { customRender: "action" },
-  },
 ];
 export default {
-  components: { EPCModal },
   data() {
     return {
       columns,
@@ -108,62 +80,6 @@ export default {
         this.pagination = pagination;
       });
     },
-    showModal(type, record) {
-      console.log(type);
-      if (type === "new") {
-        this.defaultForm = {};
-        this.editId = "";
-      }
-
-      if (type === "edit") {
-        this.defaultForm = {
-          epcNum: record.epc_num,
-          epcName: record.epc_name,
-        };
-        this.editId = record.id;
-      }
-      this.$refs.EPCModal.visible = true;
-    },
-    handleOk(record) {
-      console.log(record);
-      if (this.editId) {
-        record.epcId = this.editId;
-        this.$api.UPDATE_EPC(record).then((res) => {
-          if (res.data.code === 0) {
-            this.$message.success("编辑成功成功");
-            this.fetch();
-            this.$refs.EPCModal.visible = false;
-          } else {
-            this.$message.error(res.data.errMsg || "编辑失败，请联系管理员");
-          }
-        });
-      } else {
-        record.supplierId = this.supplierId;
-        this.$api.ADD_EPC(record).then((res) => {
-          if (res.data.code === 0) {
-            this.$message.success("新增成功");
-            this.fetch(1);
-            this.$refs.EPCModal.visible = false;
-          } else {
-            this.$message.error(res.data.errMsg || "新增失败，请联系管理员");
-          }
-        });
-      }
-    },
-    confirmDelete(id) {
-      console.log(id);
-      const data = {
-        id: id,
-      };
-      this.$api.DEL_EPC(data).then((res) => {
-        if (res.data.code === 0) {
-          this.$message.success("删除成功");
-          this.fetch(1);
-        } else {
-          this.$$message.error(res.data.errMsg || "删除失败");
-        }
-      });
-    },
   },
 };
 </script>
@@ -173,5 +89,10 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+
+.epc-list .top h2 {
+  color: #003d74;
+  margin-bottom: 20px;
 }
 </style>
